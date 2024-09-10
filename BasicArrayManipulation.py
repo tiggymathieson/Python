f50c3032c2fd7846c5181161fddbd29456552479
tiggymathieson <mathieson.acw@oundleschool.org.uk>
2024-09-10 12:00:44 +0100

    lessonwork

---
+++ BasicArrayManipulation.py
@@ -0,0 +1,11 @@
+array = []
+for i in rage [10]:
+    num = int(input(enter a number))
+    array = array.append(num)
+print(array)
+
+myarray = [0] * 10
+for i in range(10):
+    num = int(input(enter a number))
+    myarray[i] = num
+print(myarray)
